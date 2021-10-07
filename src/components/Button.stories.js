30ad8afa68c3130c5b7a0cd6522b8473db68bf52
usmaanbinnaeem <usmaanbinnaeem@gmail.com>
2021-10-07 23:38:56 +0500

    first commit

---
+++ src/components/Button.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Button from './Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Submit',
+  backgroundColor: '#EADEB8',
+  padding: '10px',
+};
